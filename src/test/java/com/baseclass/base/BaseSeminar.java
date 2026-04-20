1903e36064fd8cf5caf7a0c492e5c2ad43aafb41
chandrakantha <chandrakantha@localhost>
2026-04-20 11:16:54 +0530

    Seminar Base

---
+++ src/test/java/com/baseclass/base/BaseSeminar.java
@@ -0,0 +1,25 @@
+package com.baseclass.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseSeminar {
+	public WebDriver driver;
+
+	@BeforeMethod
+    public void setupp() throws InterruptedException {
+
+    	ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		options.addArguments("--disable-extensions");
+
+		  driver = new ChromeDriver(options);
+		// Thread.sleep(3000);
+		  driver.manage().window().maximize();
+		driver.get("https://hellomentor.in/");
+		
+        
+}
+}
